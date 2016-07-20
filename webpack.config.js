8ed94035c1d1fdc46eb2653f538461b3ab8d83f6
Scott <sekorn@gmail.com>
2016-07-20 12:17:30 -0400

    changed webpack again

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV' : process.env.NODE_ENV || 'development'
+        'NODE_ENV' : process.env.NODE_ENV
       }
     })
   ],
