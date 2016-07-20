8d987a9c2074bb67486b422a77bf633e86b79789
Scott <sekorn@gmail.com>
2016-07-20 12:14:26 -0400

    modified webpack for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
       compressor: {
         warnings: false
       }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV' : process.env.NODE_ENV || 'development'
+      }
     })
   ],
   output: {
