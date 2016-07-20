c56dc12b84c08245e6cb5f029d43a83b949c6d7a
Scott <sekorn@gmail.com>
2016-07-20 12:19:25 -0400

    changed webpack again

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,6 @@ module.exports = {
       compressor: {
         warnings: false
       }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV' : process.env.NODE_ENV
-      }
     })
   ],
   output: {
